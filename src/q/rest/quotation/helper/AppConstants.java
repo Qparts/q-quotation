c31d10f4f584fe2d77e4ef271690cd8b86e8d01a
fareed <fareed@qetaa.com>
2019-04-29 16:21:03 +0300

    added test end point for customers

--- src/q/rest/quotation/helper/AppConstants.java
+++ src/q/rest/quotation/helper/AppConstants.java
@@ -9,6 +9,7 @@ public class AppConstants {
     private final static String PUBLIC_PRODUCT_SERVICE= SysProps.getValue("productPublicService");
 
     public static final String CUSTOMER_MATCH_TOKEN = CUSTOMER_SERVICE + "match-token";
+    public static final String CUSTOMER_MATCH_TOKEN_WS = CUSTOMER_SERVICE + "match-token/ws";
     public static final String USER_MATCH_TOKEN = USER_SERVICE + "match-token";
     public static final String USER_MATCH_TOKEN_WS = USER_SERVICE + "match-token/ws";
     public static final String POST_QUOTATION_VIN_IMAGE = IMAGE_SERVICE + "quotation-vin";
